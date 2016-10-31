96d2202c177521ca01dd363b0868981668e93880
Prosen-Ghosh <prosenghosh25@gmail.com>
2016-10-31 23:51:57 +0600

    Operators Example

---
+++ Operators.js
@@ -0,0 +1,16 @@
+a = 10;
+b = 5;
+
+console.log(a+b); // Output Should Be: 10+5 = 15
+console.log(a-b); // Output Should Be: 10-5 = 5
+console.log(a*b); // Output Should Be: 10*5 = 50
+console.log(a/b); // Output Should Be: 10/5 = 2
+console.log(a%b); // Output Should Be: 10%5 = 0
+
+a += a;
+console.log(a) // Output Should Be: 10+ 10 = 20
+a *= a;
+console.log(a) // Output Should Be: 20*20 = 400
+
+a /= b;
+console.log(a) // Output Should Be: 400/5 = 80
