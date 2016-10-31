b39ec3d68a18f31126d32108a66053f80a9ef03a
Prosen-Ghosh <prosenghosh25@gmail.com>
2016-11-01 00:10:32 +0600

    Values And Types

---
+++ Values&Types.js
@@ -0,0 +1,16 @@
+str1 = "I Am A String Variable.";
+str2 = 'I Am Also A String Variable.';
+a = 10;
+numStr = "100";
+num = 100;
+
+console.log(str1);
+console.log(str2);
+console.log(a);
+console.log(numStr);
+if(numStr === num){ // === meand strict-equals
+  console.log("Both Variables Type Are Same.");
+}
+else {
+  console.log("Variables Type Are Not Same.");
+}
