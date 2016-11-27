1dab8290a00733814ce2904b943b3f602f146d37
Prosen-Ghosh <prosenghosh25@gmail.com>
2016-11-28 03:40:51 +0600

    String conversions

--- String conversions.js
+++ String conversions.js
@@ -4,8 +4,14 @@ console.log(typeof s);
 console.log(typeof n);
 console.log(n);
 
-s = "100"
-n = s*1 //convert string to a number
+s = "100";
+n = s*1; //convert string to a number
 console.log(typeof s);
 console.log(typeof n);
 console.log(n);
+
+s = "101 a";
+n = s*1; //conversion fails
+console.log(typeof s);
+console.log(typeof n);
+console.log(n); // result should NaN
