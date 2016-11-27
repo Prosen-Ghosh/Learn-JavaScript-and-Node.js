734e9903073a77c9fec6de4f8b4d4e66287fe6b3
Prosen-Ghosh <prosenghosh25@gmail.com>
2016-11-28 03:38:14 +0600

    String conversions

---
+++ String conversions.js
@@ -0,0 +1,11 @@
+var s = "10";
+var n = s/2; //convert string to a number
+console.log(typeof s);
+console.log(typeof n);
+console.log(n);
+
+s = "100"
+n = s*1 //convert string to a number
+console.log(typeof s);
+console.log(typeof n);
+console.log(n);
