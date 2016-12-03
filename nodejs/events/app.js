7be26d8cb302028c74e43e235a8e6efc2a03869e
Prosen-Ghosh <prosenghosh25@gmail.com>
2016-12-03 19:51:46 +0600

    Events

---
+++ nodejs/events/app.js
@@ -0,0 +1,20 @@
+var events = require('events');
+var util = require('util');
+var Person = require('./Person');
+
+util.inherits(Person.Person,events.EventEmitter);
+
+var Prosen = new Person.Person('Prosen');
+var Qaium = new Person.Person('Qaium');
+var Rizvi = new Person.Person('Rizvi');
+
+var people = [Prosen,Qaium,Rizvi];
+
+people.forEach(function(x){
+  x.on('say',function(msg){
+    console.log(msg);
+  });
+});
+
+Prosen.emit('say','Hello Qaium')
+Qaium.emit('say','Hello Prosen')
