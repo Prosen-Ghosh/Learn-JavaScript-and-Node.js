f9b2bdeea785c2db4ac4819979632e9724e5a914
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-01-07 05:31:04 +0600

    Stream Checked

---
+++ nodejs/stream/app.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+var random = fs.createReadStream(__dirname + '/random.txt',{encoding: 'utf8',highWaterMark: 32*1024});
+
+var randomWrite = fs.createWriteStream(__dirname + '/randomcopy.txt');
+
+random.on('data',function(chunk){
+    console.log(chunk.length);
+    randomWrite.write(chunk);
+});
+
+
+
